dad6a8c340acf8df127842b009c5f3771cf6f998
Saswat K. Levin <saswatlevin@gmail.com>
2026-09-22 14:35:38 +0530

    Removed PartialType from CreateUserDto to resolve logical error.

--- grievance_redressal_portal/src/users/dto/create-user.dto.ts
+++ grievance_redressal_portal/src/users/dto/create-user.dto.ts
@@ -1,7 +1,8 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { OmitType } from '@nestjs/swagger';
+
 import { UserUniversalDto } from './user-universal.dto';
 
-export class CreateUserDto extends PartialType (
-    OmitType(UserUniversalDto, ['user_id', 'user_date_created_at'] as const)
-)
-{}
+export class CreateUserDto extends OmitType(
+    UserUniversalDto,
+    ['user_id', 'user_date_created_at'] as const
+) {}
