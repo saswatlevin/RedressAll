5b3779cfb6a364943dddb71084299caf307fe2cf
Saswat K. Levin <saswatlevin@gmail.com>
2026-09-26 11:43:30 +0530

    Renamed search-user-by-name DTO to search-users-by-name DTO.

--- grievance_redressal_portal/src/users/dto/search-users-by-name.dto.ts
+++ grievance_redressal_portal/src/users/dto/search-users-by-name.dto.ts
@@ -1,7 +1,7 @@
 import { PickType } from "@nestjs/swagger";
 import { UserUniversalDto } from "./user-universal.dto";
 
-export class SearchUserByNameDto extends PickType (
+export class SearchUsersByNameDto extends PickType (
     UserUniversalDto,
     ['user_first_name', 'user_last_name'] as const,
 )
